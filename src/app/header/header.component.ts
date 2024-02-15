54914796285aeabd51516b121466d403dcba9165
GastonFdz <gaston.fernandez@yahoo.com>
2024-02-15 18:49:58 -0300

    quito componente que no va

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { LogoComponent } from './logo/logo.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    LogoComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
